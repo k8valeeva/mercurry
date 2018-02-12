b1fbb997785568b6c02cb8b44f10e60a41ca45ab
k8valeeva <katrin.vale@gmail.com>
2018-02-12 13:49:13 +0200

    1

--- mercury/src/components/routes/UserRoute.js
+++ mercury/src/components/routes/UserRoute.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const UserRoute = ({component: Component, ...rest}) => {
-    return (
-        <Route {...rest} render={()=> {
-            let username = JSON.parse(localStorage.getItem("username")),
-                pass = JSON.parse(localStorage.getItem("password"));
+    let username = JSON.parse(localStorage.getItem("username")),
+        pass = JSON.parse(localStorage.getItem("password"));
 
-            if (!(username === null) &&
-                !(pass === null)) {
-
-                return <Component/>
-            } else {
-
-                return <Redirect to="/login" />
-            }}}/>
-    )
+    if (!(username === null) &&
+        !(pass === null)) {
+        return (
+            <Route {...rest} render={()=> {return <Component/>}}/>
+        )
+    } else {
+        return <Redirect to="/login" />
+    }
 };
 
 export default UserRoute;
